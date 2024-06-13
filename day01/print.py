3a1a8d8af42c9f9e5cfef7a95e8beb339e28ba1a
who <283184164@qq.com>
2024-06-13 15:07:16 +0800

    first commit

---
+++ day01/print.py
@@ -0,0 +1,33 @@
+print('hello python')
+print('---------------------------')
+a = 100
+print(isinstance(a, int))
+a = b = c = 1
+print(a)
+print(b)
+print(c)
+d, e, f = 1, 2, 'yes'
+print(d)
+print(e)
+print(e)
+print(f)
+
+print('减法运算')
+print(9 - 1)
+print('除法运算')
+print(8 / 3)
+print('取余')
+print(8 % 5)
+str = 'hello'
+print('从第一个到倒数第二个')
+print(str[0: -1])
+print('from index 0 to length - 2')
+print(str[0: -2])
+print('str length')
+print(len(str))
+print('str * 3')
+print(str * 3)
+list = [1, 2, 3, 4, 5, 6]
+print(list[1])
+print(list[0])
+print(list)
