43e1a66c90d56f1623473e88b2149b59b225b9ad
who <283184164@qq.com>
2024-06-13 16:01:41 +0800

    list dict tuple

--- day01/print.py
+++ day01/print.py
@@ -1,3 +1,5 @@
+from random import random
+
 print('hello python')
 print('---------------------------')
 a = 100
@@ -31,3 +33,31 @@ list = [1, 2, 3, 4, 5, 6]
 print(list[1])
 print(list[0])
 print(list)
+sites = ['google', 'baidu', '360', 'alibaba']
+print(sites)
+if 'tencent' in sites:
+    print('in here')
+else:
+    print('not here')
+a = set('abcdefghigk')
+b = set('efg')
+print(a)
+print(a - b)
+print(int(1.00))
+print(float(1))
+a = 1
+b = 1.0
+c = 'test'
+e = [1, 2]
+dic = {}
+dic[1] = 'python'
+dic[2] = 'java'
+print('a type is', type(a))
+print('a type is ', type(b))
+print('c type is ', type(c))
+print('e type is ', type(e))
+print(type(dic))
+print(dic.values())
+print(dic.keys())
+print(dic.get(1))
+print(random() * 100)
---
+++ day02/list.py
@@ -0,0 +1,22 @@
+sites = ['baidu', '360', 'alibaba', 'taobao']
+print(sites)
+sites.append('pinduoduo')
+print(sites)
+sites.append(1)
+print(sites)
+sites.append(1.0)
+print(sites)
+dic = {}
+dic[1] = 'mayun'
+sites.append(dic)
+print(sites)
+list = [1, 2, 4, 6]
+print(max(list))
+print(min(list))
+print(list.copy())
+tup = (1, 2, 3)
+tup1 = (6, )
+print(tup)
+print(tup[1])
+print(type(tup))
+print(type(tup1))
